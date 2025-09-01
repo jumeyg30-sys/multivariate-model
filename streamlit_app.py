7061ba5f5c393323fd1598398cab3fc906b2d847
jumeyg30-sys <jumeyg30@gmail.com>
2025-09-01 16:58:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -294,7 +294,7 @@ def main() -> None:
         aves_totales = df[df['COMMON NAME'] == selected_common_name]['ALL SPECIES REPORTED'].sum().astype(int)  
         
         kpi_html = f"""
-        <div style="background-color:#FFD700::; padding: 10px 10px; border-radius: 10px; color:white; text-align:center; width: 180px; margin:auto; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);">
+        <div style="background-color:#FFD700::; padding: 10px 10px; border-radius: 10px; color:white; text-align:center; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
             <h4 style="margin:0; font-size:25px;">🐤 Total de {selected_common_name}</h4>
             <h3 style="margin:5px 0; font-size: 24px;">{aves_totales}</h3>
         </div>
